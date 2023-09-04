641275e56e91767a87ba003f555c380273831bcf
Yash Verma <officialyash23@gmail.com>
2023-09-05 01:18:25 +0530

    first commit

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import "./Join.css"
+import {Link} from "react-router-dom"
+let user;
+
+
+const sendUser=()=>{
+  user=document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value="";
+}
+const Join = () => {
+  const [name, setName] = useState("");
+  return (
+    <div className="joinPage">
+      <div className="joinContainer">
+       <h1>CHAT APP</h1>
+       <input name='name' placeholder="Enter your Name" id="joinInput" type="name" onChange={(e)=>setName(e.target.value)} />
+      <Link  onClick={(e)=>name===""? e.preventDefault():null} to="/chat">
+        <button onClick={sendUser}className="LoginBtn">Log In</button>
+        </Link> 
+      </div>
+    </div>
+  )
+}
+
+export default Join
+export{user}
